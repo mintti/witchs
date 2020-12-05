446532ebb1882a56c36340405f55691d8f7234ad
Mintti <mintti97@gmail.com>
2020-12-06 08:42:01 +0900

    Modify Cooking

--- _Game/Module/Cooking.cs
+++ _Game/Module/Cooking.cs
@@ -1,36 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Model.Cook;
+using Model.Cooking;
 
 public class Cooking : MonoBehaviour
 {
 
-    private List<Recipe> Recipes;
+    private Recipe Recipes;
     // Start is called before the first frame update
     void Start()
     {
         SetRecipe();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void SetRecipe()
     {
-        Recipes.Add(new Recipe(new List<int>(){1, 2, 3}, 10));
+        Recipes.Add(new List<int>(){1, 2, 3},10);
     }
 
-    public int? Cook(List<int> ele)
+    public int Cook(List<int> ele)
     {
         ele.Sort();
-        int? result = Recipes.Find(e => e.Ingredients.Equals(ele)).Result;
 
-        if(result == null)
-            return -1;
-        return result;
+        return Recipes.Get(ele);
     }
 }
---
+++ _Infra/Model/Recipe.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Model.Cooking
+{
+    public class Recipe
+    {
+        public List<Recipe> Tail;
+        public int _value;
+        public int _result;
+
+        public Recipe(int value)
+        {
+            _value = value;
+        }
+
+        public void Add(List<int> list, int res)
+        {
+            _value = list[0];
+            list.RemoveAt(0);
+
+            if(list.Count == 0)
+                _result = res;
+            else
+            {
+                if(Tail.Find(e => e._value.Equals(list[0])).Equals(null))
+                {
+                    Tail.Add(new Recipe(list[0]));
+                }
+                Tail.Find(e => e._value.Equals(list[0])).Add(list, res);
+            }
+        }
+        
+        public int Get(List<int> list)
+        {
+            Recipe node = Tail.Find(e => e._value.Equals(list[0]));
+            list.RemoveAt(0);
+
+            if(node.Equals(null)) return -1;
+
+            if(list.Count > 1)
+                return node.Get(list);
+            else return node._result;
+        }
+    }
+}
