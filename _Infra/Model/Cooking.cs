64a9307a8b51234b08f0b5f0c27366969ee2ef59
Mintti <mintti97@gmail.com>
2020-12-06 01:32:14 +0900

    Cooking

---
+++ _Infra/Model/Cooking.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Model.Cook
+{
+    public class Recipe
+    {
+        public Node<int> Ingredients;
+        public int Result;
+
+        public Recipe(List<int> ing, int res)
+        {
+            Ingredients = new Node<int>();
+            Ingredients.Add(ing);
+
+            Result = res;
+        }
+    }
+    
+    public class Node<T>
+    {
+        public Node<T> Tail;
+        public T _value;
+        
+        public void Add(List<T> value)
+        {
+            _value = value[0];
+            value.RemoveAt(0);
+
+            if(value.Count == 0)
+                return;
+            else
+            {
+                Tail = new Node<T>();
+                Tail.Add(value);
+            }
+            
+        }
+    }
+}
